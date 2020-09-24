7a37f7af632e536327cf226fd6f4f5792ae23fa4
CyperNastee <55226758+CyperNastee@users.noreply.github.com>
2020-09-24 16:36:25 +0300

    test2

--- Workshop 3/koodit.js
+++ Workshop 3/koodit.js
@@ -120,3 +120,41 @@ function focusFunction(x) {
     document.getElementById("textdata").placeholder="Please fill in the form with proper data.";
     
   }
+function checkFunction(){
+  var x = document.getElementById("textdata").value;
+  if (x == null || x == "" || x.length < 3){
+    alert("Please fill in the form with proper data.");
+    return false;
+  }
+}
+// Excercise 5
+function showXY(event){
+  var x = event.clientX;
+  var y = event.clientY;
+  var coords = "X coords: " + x + ", Y coords: " + y;
+  document.getElementById("coordinates").innerHTML ="Mouse coordinates: " + coords;
+}
+// Excercise 6
+var x = document.getElementById("textloc");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+function showPositionmap(position) {
+  var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+  var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=
+  "+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_KEY";
+
+  document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+}
